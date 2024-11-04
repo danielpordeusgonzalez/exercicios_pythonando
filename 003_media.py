a6247742b60422c48aa4df5715acfef05c9c12d6
danielpordeusg <danielpordeusgonzalez@hotmail.com>
2024-11-04 11:44:34 -0300

    feat: sobe o exercício media completo

---
+++ 003_media.py
@@ -0,0 +1,8 @@
+#Escreva um programa aonde o usuário digite duas notas e ele mostre a média das duas notas
+
+nota1 = float(input('digite a primeira nota: '))
+nota2 = float(input('digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A primeira nota do aluno é {nota1}, a segunda nota é {nota2}, e a média do aluno é {media}')
