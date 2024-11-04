7fd779202766a0df81d2f4f8c3a477d90de15806
danielpordeusg <danielpordeusgonzalez@hotmail.com>
2024-11-04 16:32:31 -0300

    feat: imprimi na tela se o numero é positivo, negativo ou neutro

---
+++ 006_exiba_numero_positivo_negativo.py
@@ -0,0 +1,8 @@
+numero = float(input('digite um número e ver se ele é positivo ou negativo: '))
+
+if numero >0:
+    print(f'o {numero} é positivo')
+elif numero == 0:
+    print(f'o {numero} é neutro')
+else:
+    print(f'o {numero} é negativo')
