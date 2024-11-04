5bcfb22f8f5e52bac6971eec5d495f1185a680f0
danielpordeusg <danielpordeusgonzalez@hotmail.com>
2024-11-04 16:32:56 -0300

    feat: confere se a pessoa é do sexo Masculino ou Feminino

---
+++ 007_masculino_feminino.py
@@ -0,0 +1,8 @@
+sexo = str(input('Digite F se for Feminino ou M para Masculino: '))
+
+if sexo == 'F' or sexo == 'f':
+    print('Essa pessoa é do sexo Feminino.')
+elif sexo == 'M' or sexo == 'm':
+    print('Essa pessoa é do sexo Masculino.')
+else:
+    print('Valor inválido')
