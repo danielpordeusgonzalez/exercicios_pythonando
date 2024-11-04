de7133b6b6450f6d638c98a9921ad71bcb2878ce
danielpordeusg <danielpordeusgonzalez@hotmail.com>
2024-11-04 16:33:22 -0300

    feat: conversor de temperatura farenheit para Celsius

---
+++ 008_conversor_temperatura.py
@@ -0,0 +1,5 @@
+temperatura = float(input('Digite a tempetura em farenheit para convertar para graus Celsius: '))
+
+conversao = (temperatura - 32) / 1.8
+
+print(f'a conversão de {temperatura:.1f} farenheit é igual há {conversao:.1f} Celsius')
